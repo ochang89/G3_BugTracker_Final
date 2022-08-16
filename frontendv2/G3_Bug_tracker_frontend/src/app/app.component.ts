40f9dc0a418693973bd2daf61793bcfe6af00cc6
OliverChang <ochang89@gmail.com>
2022-08-16 16:02:08 -0400

    css/html & homepage updates

--- frontendv2/G3_Bug_tracker_frontend/src/app/app.component.ts
+++ frontendv2/G3_Bug_tracker_frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Ticket } from './ticket';
+import { TicketService } from './ticket.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bug-tracker';
   icon = "./assets/bug_icon.png";
-}
+  tickets?: Ticket[];
+  ticket: Ticket = new Ticket();
+  id!: number;
+
+  constructor(private ticketService: TicketService,
+    private route: ActivatedRoute,
+    private router: Router){
+  }
+
+  ngOnInit(): void {
+    this.getTicket();
+  }
+
+  private getTicket(){
+    this.ticket = new Ticket();
+    this.ticketService.getTicketById(this.ticket.id).subscribe( data => {
+      this.ticket = data;}
+    )}
+
+  ticketDetails(id:number){
+    // navigate to ticket-details component
+    this.router.navigate(['ticket-details', id]);
+  }
+
+  goHome(){
+    // navigate to home page
+    this.router.navigate(['home-page']);
+  }}
